2aceb3d3165c69682b6d21f6306ac0ce949e3749
jinglis-all-the-way <joinwill123@gmail.com>
2026-04-16 15:21:04 -0400

    Error printing

--- TacoShell.py
+++ TacoShell.py
@@ -47,6 +47,8 @@ class TacoShell(cmd2.Cmd):
                     
                     plugin_kwargs = self._parse_plugin_args(args.plugin_args)
                     plugin_instance = plugin_class(**plugin_kwargs)
+                    plugin_instance._shell = self
+                    print(f"Plugin : {plugin_instance.name}")
                     
                     # Check if this plugin wants to be the default handler
                     is_default_handler = plugin_instance.default != BasePlugin.default
